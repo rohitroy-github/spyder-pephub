0fbbca909619a3c37adbb60856679bfe296f0972
rohit <rhtry.tech@gmail.com>
2023-02-19 15:50:50 +0530

    netlify issue

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 import BlogPageContent from "../components/BlogPageContent";
