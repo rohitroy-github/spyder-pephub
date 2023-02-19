605606e87eb2f34659ed64c45ae8b394b38b9854
rohit <rhtry.tech@gmail.com>
2023-02-19 12:01:40 +0530

    Footer structure donw !

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div>
+        {/* topFooterPartDiv */}
+        <div>
+          {/* div-1 */}
+          <div>
+            <h1>SDE Prep</h1>
+            {/* socialIconsDiv */}
+            <div>
+              <img></img>
+              <img></img>
+              <img></img>
+            </div>
+            <p>join our newsletter for daily updates!</p>
+            {/* subscribeSection */}
+            <div>
+              <input></input>
+              <button></button>
+            </div>
+          </div>
+
+          {/* div-2 */}
+          <div>
+            <p>Blogs</p>
+            <p>Interview Preparation</p>
+            <p>Interview Experience</p>
+            <p>Write for Us</p>
+            <p>About Us</p>
+          </div>
+
+          {/* div-3 */}
+          <div>
+            <p>Resource</p>
+            <p>Resource</p>
+            <p>Resource</p>
+            <p>Resource</p>
+            <p>Resource</p>
+            <p>Resource</p>
+          </div>
+        </div>
+
+        {/* middleSeparatorLine */}
+        <div></div>
+
+        {/*bottomFooterPart */}
+        <div>
+          <p>© The Clever World. All rights reserved.</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
