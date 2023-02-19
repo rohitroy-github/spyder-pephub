b5d06f7442ae17157ecd8824fd3a8589e9ae53fa
rohit <rhtry.tech@gmail.com>
2023-02-19 12:30:34 +0530

    Footer structure donw !

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
+import joinbg from "../../assets/code_pattern 1.svg";
+
 const Join = () => {
   return (
     <>
-      <section>
-        <div>
-          <div>
-            Share your knowledge.
-            <br />
-            Write for PepHub.
+      <section className="join-section flex jutify-around">
+        <div className="pl-16 ">
+          <div className="join-text pt-14">
+            <h1 className="text-5xl font-semibold leading-[3rem]">
+              Share your knowledge.
+              <br />
+              Write for PepHub
+            </h1>
+
+            <h2 className="text-2xl pt-8">
+              Write blogs for us, share your knowledge & experience. <br /> Help
+              thousands like you prepare better for their goals.
+            </h2>
+
+            <button className="pt-8">Start writing now!</button>
           </div>
-          <p>
-            Write blogs for us, share your knowledge & experience. Help
-            thousands like you prepare better for their goals.
-          </p>
-          <button>Start writing now!</button>
         </div>
-        <div>
-          <p>Topics you can write on</p>
+        <div className="pt-10">
+          <h1 className="text-2xl font-medium ">Topics you can write on</h1>
+
           <div>Cognizant Digital Nurture Technical Interview Experience</div>
           <div>Cognizant Digital Nurture HR Interview Experience</div>
           <div>TCS Digital Technical Interview Experience</div>
